82a9f2716433a1f43777ffc050e3c25321bf28ac
MRAsoft <info.mrasoft@gmail.com>
2021-08-17 19:34:23 +0200

    Add project files.

---
+++ Server/Controllers/BerekeningController.cs
@@ -0,0 +1,33 @@
+﻿using ErrorHandling.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BerekeningController : ControllerBase
+    {
+        [HttpPost]
+        [Route("DoeBerekening")]
+        public async Task<ActionResult<Berekening>> DoeBerekening(Berekening berekening)
+        {
+            try
+            {
+                berekening.Uitkomst = Math.Round(berekening.Teller / berekening.Noemer, 2);
+                await Task.Delay(0);
+                return Ok(berekening);
+            }
+            catch(Exception e)
+            {
+                return Problem(statusCode: (int)HttpStatusCode.InternalServerError, 
+                               detail: "Er is iets fout gegaan met uw berekening: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Shared/Berekening.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ErrorHandling.Shared
+{
+    public class Berekening
+    {
+        public int Teller { get; set; }
+        public decimal Noemer { get; set; }
+        public decimal Uitkomst { get; set; }
+    }
+}
